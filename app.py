5e93ea48ed0ac7cd51533a1a5f3135525bec47dc
Ryu-Okamoto <r-okamot@ist.osaka-u.ac.jp>
2024-12-09 22:19:34 +0900

    test

--- app.py
+++ app.py
@@ -1,9 +1,61 @@
-from flask import Flask
+import os
+from flask import (
+    Flask,
+    request,
+    abort
+)
+from dotenv import load_dotenv
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    ApiClient,
+    Configuration,
+    MessagingApi,
+    ReplyMessageRequest,
+    PushMessageRequest,
+    TextMessage,
+    PostbackAction
+)
+from linebot.v3.webhooks import (
+    FollowEvent,
+    MessageEvent,
+    PostbackEvent,
+    TextMessageContent
+)
 
+
+load_dotenv()
+CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
 app = Flask(__name__)
 
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info(
+            "Invalid signature."
+            "Please check your channel access token/channel secret."
+        )
+        abort(400)
+
+    return 'OK'
+
+
+@app.route('/', methods=['GET'])
+def toppage():
+    return 'Hello world!'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
